b433dc4aae29ca6cf71963c24e2e6ae2b1eb98f4
JongReulKang <cheerjong12@naver.com>
2021-10-14 23:48:26 +0900

    activity_quiz_main.xml, DifficultyActivity.java 수정

--- app/src/main/java/com/course/kpop/DifficultyActivity.java
+++ app/src/main/java/com/course/kpop/DifficultyActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.media.AudioManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.Spannable;
